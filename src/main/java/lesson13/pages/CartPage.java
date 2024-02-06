7bb297c5b814113d4d26e687d63fdff977c7aece
hordiienkod <dmytro.hordiienko@appflame.com>
2024-02-06 13:27:47 +0200

    lesson13 (fixed)

--- src/main/java/lesson13/pages/CartPage.java
+++ src/main/java/lesson13/pages/CartPage.java
@@ -12,16 +12,20 @@ public class CartPage extends MainPage {
     private ElementsCollection itemPrice = $$(".inventory_item_price");
     private SelenideElement checkoutButton = $("#checkout");
 
+   public void assertItsCartPage(){
+       String url = WebDriverRunner.url();
+       Assert.assertEquals(url, "https://www.saucedemo.com/cart.html", "It's not a cart page!");
+   }
     public void assertItems() {
         Boolean found799 = null;
         Boolean found999 = null;
         for (int i = 0; i < itemPrice.size(); i++) {
             String priceText = itemPrice.get(i).text();
             if (priceText.equals("$7.99")) {
-                Assert.assertEquals(priceText, "$7.99", "PRIDUROK");
+                Assert.assertEquals(priceText, "$7.99", "Price isn't equal to $7.99 ");
                 found799 = true;
             } else if (priceText.equals("$9.99")) {
-                Assert.assertEquals(priceText, "$9.99", "PRIDUROK");
+                Assert.assertEquals(priceText, "$9.99", "Price isn't equal to $9.99");
                 found999 = true;
             }
         }
--- src/main/java/lesson13/pages/OverviewPage.java
+++ src/main/java/lesson13/pages/OverviewPage.java
@@ -8,9 +8,6 @@ import org.testng.Assert;
 import static com.codeborne.selenide.Selenide.$;
 
 public class OverviewPage {
-    private final String firstName = "Valeriy";
-    private final String lastName = "Zhmyshenko";
-    private final String postalCode = "12345";
 
     private SelenideElement firstNameField = $("#first-name");
 
@@ -22,19 +19,25 @@ public class OverviewPage {
     private SelenideElement continueButton = $("#continue");
 
 
-    public void fillPersonalData() {
+    public void fillFirstName(String firstName) {
         firstNameField.shouldBe(Condition.visible).shouldBe(Condition.editable).val(firstName);
         String firstNameval = firstNameField.getAttribute("value");
         Assert.assertEquals(firstNameval, firstName, "The first name isn't equals");
+    }
 
+    public void fillLastName(String lastName) {
         lastNameField.shouldBe(Condition.visible).shouldBe().val(lastName);
         String lastNameVal = lastNameField.getAttribute("value");
         Assert.assertEquals(lastNameVal, lastName, "The last name isn't equals");
+    }
 
+    public void fillPostalCode(String postalCode) {
         postalCodeField.shouldBe(Condition.visible).shouldBe(Condition.editable).val(postalCode);
         String postalCodeVal = postalCodeField.getAttribute("value");
         Assert.assertEquals(postalCodeVal, postalCode, "The postal code isn't equals");
+    }
 
+    public void continueForward() {
         continueButton.shouldBe(Condition.visible).shouldBe().click();
         String url = WebDriverRunner.getWebDriver().getCurrentUrl();
         Assert.assertEquals(url, "https://www.saucedemo.com/checkout-step-two.html", "Redirect to the step two failed");
